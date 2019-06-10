f0d83ef83349be3c286f5cd7e12d56fe4d5e92a1
htsume <hashimasa0503@gmail.com>
2019-06-10 15:18:57 +0900

    ディレクトリ変更中

--- read/views.py
+++ read/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request, 'read/index.html')
+	params = {
+		'search':'検索とかそういうの - 開発中',
+	}
+	return render(request, 'read/index.html', params)
--- u_message/settings.py
+++ u_message/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'u_message.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'u_message/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(os.path.dirname(__file__), '/u_message/static'),
+
+]
