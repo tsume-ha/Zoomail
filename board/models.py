a71e5b4b668115082e6e67dd39f5379dcf9ef474
htsume <hashimasa0503@gmail.com>
2019-06-21 14:44:08 +0900

    Messages とMessage_Yearを作成

--- board/models.py
+++ board/models.py
@@ -10,4 +10,21 @@ class PostTest(models.Model):
 
 	def __str__(self):
 		return 'post_ID=' + str(self.id) + ', title=' + self.title
+
+
+class Messages(object):
+	"""docstring for Messages"""
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+	attachment = models.BooleanField(defalt=False)
+		#添付ファイル
+	sender_id = IntegerField()
+	writer_id = IntegerField()
+		#転載時に使用。
+		#文章を書いた人がwriter、アップロードした人がsender。
+		#通常なら sender == writer で同じになる。
+	created_at = DateTimeField()
+	updated_at = DateTimeField()
+	def __init__(self):
+		return 'mes_ID=' + str(self.id) + ', title=' + self.title
 		
