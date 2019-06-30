21b53d8861724d6921968cc2121d2faaac5c076a
htsume <hashimasa0503@gmail.com>
2019-06-30 21:08:07 +0900

    admin修正忘れを修正

--- board/admin.py
+++ board/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Message, Message_Year, Message_Attachment, Message_Tag
+from .models import Messages, Message_Year, Message_Attachment, Message_Tag
 
-admin.site.register(Message)
+admin.site.register(Messages)
 admin.site.register(Message_Year)
 admin.site.register(Message_Attachment)
 admin.site.register(Message_Tag)
