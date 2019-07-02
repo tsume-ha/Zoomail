c3ea014c45458138dfbfacb88a8ce830eea6156b
htsume <hashimasa0503@gmail.com>
2019-07-03 01:11:31 +0900

    userのmodelを既存のものから変更中 あとはadmin.pyを書き換えればおーけー
    DB, migrationファイルを削除して作り直す必要有り
    順番は
    1.全削除
    2.db.sqlite3作成
    3.membersにだけmakemigration(python manage.py makemigrations members)
    4.membersにだけmigrate(python manage.py migrate members)
    5.全体にmakemigration
    6.全体にmigrate
    7.superuser作成(python manage.py createsuperuser)

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,6 @@ from . import settings_local
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-print("setting.py - " + BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -107,8 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+
 # CustomUserModel
 AUTH_USER_MODEL = 'members.User'
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- members/models.py
+++ members/models.py
@@ -1,7 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+	def create_superuser(self, google_account, password=None):
+		user = self.create_user(
+			google_account,
+			password=password,
+		)
+		user.is_admin = True
+		user.save(using=self._db)
+		return user
+
+class User(AbstractBaseUser, PermissionsMixin):
 	google_account = models.EmailField(unique=True)
 	first_name = models.CharField(max_length=255)
 	last_name = models.CharField(max_length=255)
@@ -10,8 +21,7 @@ class User(AbstractUser):
 	created_at = models.DateTimeField()
 	updated_at = models.DateTimeField()
 
-	EMAIL_FIELD = 'email'
-	USERNAME_FIELD = 'email'
+	USERNAME_FIELD = 'google_account'
 	REQUIRED_FIELDS = []
 
 	def __str__(self):
