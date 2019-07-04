0437004e4c94fae0a40f033c101faa82773bb17e
htsume <hashimasa0503@gmail.com>
2019-07-04 23:08:34 +0900

    admin.py 編集中

--- members/admin.py
+++ members/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
 from .models import User
 
 @admin.register(User)
-class AdminUserAdmin(UserAdmin):
+class UserAdmin(BaseUserAdmin):
 	fieldsets = (
-		(none, {'fields': ('')})
+		(None, {'fields': ('google_account', 'year')}),
+		('Personal info'), {'fields': ('first_name', 'last_name', 'google_account')}),
+		('Permissions', {'fields': ('is_admin', )}),
 		)
+	search_fields = ('google_account',)
+	ordering = ('google_account',)
+	filter_horizontal = ()
+
+admin.site.unregister(Group)
