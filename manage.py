c6025c0c63119eeb609863de569ff382c9c9dd7d
htsume <hashimasa0503@gmail.com>
2019-08-25 23:02:14 +0900

    db change

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@
 import os
 import sys
 
+if config.settings_local.DEBUG != True:    
+    import pymysql
+    pymysql.install_as_MySQLdb()
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
