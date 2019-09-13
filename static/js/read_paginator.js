ecb9ebfb919fdc35a229cbebdaaa97734cedb551
htsume <hashimasa0503@gmail.com>
2019-09-14 01:32:33 +0900

    ページナビゲーションの現在のページをdisabledにする処理を追加

--- static/js/read_paginator.js
+++ static/js/read_paginator.js
@@ -20,3 +20,23 @@ var infScroll = new InfiniteScroll( '.content_wrapper', {
     scrollThreshold: 300,
 
 });
+
+
+
+$(window).on('load scroll resize',function(){
+    var path = location.search;
+    var now_page = path.slice(path.indexOf("?page=")+6)
+    if (now_page == '') {
+        now_page = 1;
+    }
+    var target = "li[data-page=" + now_page + "]"
+    $('ul.pagination li').each(function(){
+        $(this).removeClass('disabled')
+        $(this).children('a').addClass('text-primary')
+        $(this).children('a').removeClass('text-secondary disabled')
+    });
+    $(target).addClass('disabled')
+    $(target).children('a').addClass('text-secondary disabled')
+    $(target).children('a').removeClass('text-primary')
+});      
+
