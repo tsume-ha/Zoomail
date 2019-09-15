59c9a71e8a1269e70092bd31c4676fda63be2dc7
htsume <hashimasa0503@gmail.com>
2019-09-15 22:54:43 +0900

    とりあえず表示

--- config/urls.py
+++ config/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
     path('player/', include('player.urls')),
     path('kansou/', include('kansou.urls')),
     path('pictures/', include('pictures.urls')),
+    path('others/', include('otherdocs.urls')),
 ]
--- otherdocs/views.py
+++ otherdocs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required()
+def index(request):
+
+    params = {
+    }
+    return render(request, 'otherdocs/index.html', params)
