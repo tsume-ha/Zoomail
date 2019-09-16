9eda765835dfb406edae6e74bfd01b026f246433
htsume <hashimasa0503@gmail.com>
2019-09-16 21:21:08 +0900

    modelも作成

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from members.models import User
 
-# Create your models here.
+class YoutubeURL(models.Model):
+    title = models.CharField(max_length=200, verbose_name='イベント名')
+    listid = models.CharField(max_length=200, blank=True, verbose_name='Youtubeの再生リストURL')
+    textcontent = models.TextField(max_length=2000, blank=True, verbose_name='テキスト')
+    held_at = models.DateField(verbose_name='イベントを行った日')
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='movie_creater')
+    def __str__(self):
+        return self.held_at.strftime('%Y-%m-%d') + ' : ' + self.title
