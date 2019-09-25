63d900189aadca17d7430c78c88a470d3804f2e1
htsume <hashimasa0503@gmail.com>
2019-09-25 22:02:24 +0900

    otherdocsのtestを追加

--- otherdocs/tests.py
+++ otherdocs/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from members.models import User
 
-# Create your tests here.
+
+def User_LogOUT(self):
+    self.client = Client()
+    self.client.logout()
+
+def Make_User(self,year=2019):
+    self.user = User.objects.create_user(email=str(year) + 'mail@gmail.com', year=year)
+    return self.user
+
+def User_LogIN(self,year=2019):
+    self.user = User.objects.get(email=str(year) + 'mail@gmail.com')
+    self.client.force_login(self.user)
+    return self.user
+
+
+class OtherDocumentsViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Make_User(cls)
+
+    def test_other_docs_view_logOUT(self):
+        User_LogOUT(self)        
+        response = self.client.get('/others/')
+        self.assertEqual(response.status_code, 302)
+        url_redial_to = response.url
+        response = self.client.get(url_redial_to)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'admin/login.html')
+
+    def test_other_docs_view_logIN(self):
+        User_LogIN(self)
+        response = self.client.get('/others/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'otherdocs/index.html')
