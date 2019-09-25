d02fc9771fdb9887dae6c585e2253d212008c795
htsume <hashimasa0503@gmail.com>
2019-09-25 21:58:51 +0900

    otherdocsは（とりあえず）HTMLへ直に書くことにします

--- otherdocs/views.py
+++ otherdocs/views.py
@@ -3,7 +3,11 @@ from django.contrib.auth.decorators import login_required
 
 @login_required()
 def index(request):
-
+# その他資料はすべてHTMLに書き出します。
+# /templates/otherdocs/index.html にどんどん書いていきましょう
+# ファイルは /private_media/others/ 以下に「英数字ファイル名」で、
+# FTPクライアントや、SSHクライアントソフトでサーバーに直接アップロードしてください。
+# (FFFTPとかでいいです)
     params = {
     }
     return render(request, 'otherdocs/index.html', params)
