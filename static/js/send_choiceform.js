6cd46669f791f4e1d6ee8f3e807a0876a468ef50
htsume <hashimasa0503@gmail.com>
2019-09-27 17:30:41 +0900

    JS修正

--- static/js/send_choiceform.js
+++ static/js/send_choiceform.js
@@ -2,7 +2,7 @@ $(function(){
     $('#id_written_by option').each(function(index, element){
         $(element).hide();
     });
-    $('#id_written_by option[value^=user_year]').each(function(index, element){
+    $('#id_written_by option[value^='+user_year+']').each(function(index, element){
         $(element).show();
     });
 })
@@ -10,11 +10,10 @@ $(function(){
 $('#id_year_choice').change(function(){
     selected_year = $(this).val();
     selected_year = String(selected_year);
-    console.log(selected_year);
     $('#id_written_by option').each(function(index, element){
         $(element).hide();
     });
-    $('#id_written_by option[value^=selected_year]').each(function(index, element){
+    $('#id_written_by option[value^='+selected_year+']').each(function(index, element){
         $(element).show();
     });
 })
