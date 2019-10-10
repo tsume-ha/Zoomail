7dd7f50bad4ec2cd72bf6d24bda59ba257785084
htsume <hashimasa0503@gmail.com>
2019-10-10 18:52:39 +0900

    div並べて作ってる

--- awase/views.py
+++ awase/views.py
@@ -4,5 +4,6 @@ from django.contrib.auth.decorators import login_required
 @login_required()
 def index(request):
     params = {
+        'display_time': (n for n in range(9,27)),
     }
     return render(request, 'awase/index.html', params)
