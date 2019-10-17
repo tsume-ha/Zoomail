edd5322a687e1e4c471f272a8cf9b2e82d4e0bb9
htsume <hashimasa0503@gmail.com>
2019-10-17 21:22:22 +0900

    Sol #59

--- static/js/ajax_bookmark.js
+++ static/js/ajax_bookmark.js
@@ -1,5 +1,11 @@
 function bookmark(message_pk) {
-    data_text = 'csrfmiddlewaretoken=' + $('input[name="csrfmiddlewaretoken"]').val() + '&message_pk=' + message_pk;
+    var data_text = 'csrfmiddlewaretoken=' + $('input[name="csrfmiddlewaretoken"]').val() + '&message_pk=' + message_pk;
+    var before_ajax = $('img#img_'+message_pk).attr('src');
+    if (before_ajax == '/static/img/star_yl.png') {
+        $('img#img_'+message_pk).attr('src', '/static/img/star_bk.png');
+    } else {
+        $('img#img_'+message_pk).attr('src', '/static/img/star_yl.png');
+    }
     $.ajax({
          url: location.protocol + '//' + location.host + '/read/ajax_bookmark/' + message_pk + '/',
          method: 'POST',
@@ -8,7 +14,7 @@ function bookmark(message_pk) {
          dataType: "text",
     })
     .done( function(data) {
-        is_marked = data.replace('bookmark=', '')
+        var is_marked = data.replace('bookmark=', '')
         if (is_marked == 'true') {
             $('img#img_'+message_pk).attr('src', '/static/img/star_yl.png');
         } else {
@@ -16,5 +22,7 @@ function bookmark(message_pk) {
         }
     })
     .fail(function() {
+        $('img#img_'+message_pk).attr('src', before_ajax);
+        console.log('通信失敗：ブックマーク処理');
     });
 }
