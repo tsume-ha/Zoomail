dbefcf09daaaa1920aa9613213db17eaccc558e8
htsume <hashimasa0503@gmail.com>
2019-10-20 21:36:04 +0900

    入力のjsはこんな感じ？

--- static/js/awase_input.js
+++ static/js/awase_input.js
@@ -1,7 +1,11 @@
 $('label[for*=can_attend]').click(function(){
-	console.log('done');
-	// var val = $(this).val();
-	// var for_attr = $(this).attr('for');
-	// console.log(for_attr);
-
+	var for_attr = $(this).attr('for');
+	var val = $('#'+for_attr).val();
+	var attr_list = for_attr.split('-');
+	if (Number(attr_list[1]) % 2 == 0) {
+		attr_list[1] = String(Number(attr_list[1]) + 1);
+		var target = attr_list.join('-');
+		$('#'+target).prop('checked', true);
+		$('#'+target.slice(0.-2)).val(val);
+	}
 })
