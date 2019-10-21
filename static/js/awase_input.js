5003245e059753fe99581934173bfa51fbd520b7
htsume <hashimasa0503@gmail.com>
2019-10-22 02:14:56 +0900

    めんどくなったので30分おきに入力に

--- static/js/awase_input.js
+++ static/js/awase_input.js
@@ -1,26 +1,27 @@
-$('label[for*=can_attend]').click(function(){
-    var for_attr = $(this).attr('for');
-    var val = $('#'+for_attr).val();
-    var attr_list = for_attr.split('-');
-    if (Number(attr_list[1]) % 2 == 0) {
-        attr_list[1] = String(Number(attr_list[1]) + 1);
-        var target = attr_list.join('-');
-        $('#'+target).prop('checked', true);
-        $('#'+target.slice(0.-2)).val(val);
-    }
-})
+// $('label[for*=can_attend]').click(function(){
+//     var for_attr = $(this).attr('for');
+//     var val = $('#'+for_attr).val();
+//     var attr_list = for_attr.split('-');
+//     if (Number(attr_list[1]) % 2 == 0) {
+//         attr_list[1] = String(Number(attr_list[1]) + 1);
+//         var target = attr_list.join('-');
+//         $('#'+target).prop('checked', true);
+//         $('#'+target.slice(0.-2)).val(val);
+//     }
+// })
 
-$('.form_one_day .form-group:nth-of-type(2n)').click(function(){
-    $(this).css('height', 'auto');
-    $(this).find('*').css('visibility','visible');
-})
+// $('.form_one_day .form-group:nth-of-type(2n)').click(function(){
+//     $(this).css('height', 'auto');
+//     $(this).find('*').css('visibility','visible');
+// })
 
 
-$(function(){
-    $('.form_one_day .form-group:nth-of-type(2n)').each(function(index, element){
-        if ($(element).is(':checked')) {} else {
-            console.log(index);
-            $(element).addClass('hidden_form-group');
-        }
-    });
-})
+// $(function(){
+//     $('.form_one_day .form-group:nth-of-type(2n)').each(function(index, element){
+//         if ($(element).is(':checked')) {} else {
+//             console.log(index);
+//             $(element).addClass('hidden_form-group');
+//         }
+//     });
+// })
+
