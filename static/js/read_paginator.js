598c0e04674fb17193c8d825082b4f85312f6c3b
htsume <hashimasa0503@gmail.com>
2019-10-23 16:24:25 +0900

    メニューのページネーションはjs依存です

--- static/js/read_paginator.js
+++ static/js/read_paginator.js
@@ -9,7 +9,7 @@ $(function(){
 })
 
 var infScroll = new InfiniteScroll( '.content_wrapper', {
-    debug: true,
+    debug: false,
     path: '.next',
     append: '.content',
     checkLastPage: true,
@@ -20,21 +20,50 @@ var infScroll = new InfiniteScroll( '.content_wrapper', {
 
 });
 
+$(window).on('load scroll resize',function(){
+    var page = infScroll.pageIndex;
+    $('ul.pagination li').each(function(){
+        $(this).removeClass('disabled')
+        $(this).children('a').addClass('text-primary')
+        $(this).children('a').removeClass('text-secondary disabled')
+    });
+    $('li[data-num=' + page + ']').addClass('disabled')
+    $('li[data-num=' + page + ']').children('a').addClass('text-secondary disabled')
+    $('li[data-num=' + page + ']').children('a').removeClass('text-primary')
+    if (page < 4) {
+        $('.first').css('display', 'none');
+        $('#omit1').css('display', 'none');
+        $('#omit2').css('display', 'inline-block');
+        $('.last').css('display', 'inline-block');
+        for (var i = 1; i <= 5; i++) {
+            $('li[data-num=' + i + ']').css('display', 'inline-block');
+        }
+        for (var i = 6; i <= page_num; i++) {
+            $('li[data-num=' + i + ']').css('display', 'none');
+        }
+    } else if (4 <= page < page_num - 3) {
+        $('.first').css('display', 'inline-block');
+        $('#omit1').css('display', 'inline-block');
+        $('#omit2').css('display', 'inline-block');
+        $('.last').css('display', 'inline-block');
+        for (var i = 1; i < page - 2; i++) {
+            $('li[data-num=' + i + ']').css('display', 'none');
+        }
+        for (var i = page - 2; i < page + 3; i++) {
+            $('li[data-num=' + i + ']').css('display', 'inline-block');
+        }
+        for (var i = page + 3; i <= page_num; i++) {
+            $('li[data-num=' + i + ']').css('display', 'none');
+        }
 
-
-// $(window).on('load scroll resize',function(){
-//     var page = location.pathname.slice(6, -1);
-//     if (page == '') {
-//         page = 1;
-//     }
-//     var target = "li[data-page=" + page + "]"
-//     $('ul.pagination li').each(function(){
-//         $(this).removeClass('disabled')
-//         $(this).children('a').addClass('text-primary')
-//         $(this).children('a').removeClass('text-secondary disabled')
-//     });
-//     $(target).addClass('disabled')
-//     $(target).children('a').addClass('text-secondary disabled')
-//     $(target).children('a').removeClass('text-primary')
-// });      
+    } else {
+        $('.first').css('display', 'inline-block');
+        $('#omit1').css('display', 'inline-block');
+        $('#omit2').css('display', 'none');
+        $('.last').css('display', 'none');
+        for (var i = 1; i < page - 4; i++) {
+            $('li[data-num=' + i + ']').css('display', 'none');
+        }
+    }
+});      
 
