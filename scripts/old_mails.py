597f66cd200cb2b94c58c6da5ee412391ab0db37
htsume <hashimasa0503@gmail.com>
2019-11-09 17:14:20 +0900

    500-600

--- scripts/old_mails.py
+++ scripts/old_mails.py
@@ -7,7 +7,7 @@ from members.models import User
 
 def run():
     admin = User.objects.get(email='developer@ku-unplugged.net')
-    for i in range(400,500):
+    for i in range(500,600):
         name = str(i) + '.eml'
         target = os.path.join(BASE_DIR, 'scripts', 'emails', name)
         try:
