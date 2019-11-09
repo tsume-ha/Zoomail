f5b4a0d1d034832e2d114cede0cdb9fba7d103b3
htsume <hashimasa0503@gmail.com>
2019-11-09 16:06:49 +0900

    killされたので短縮

--- scripts/all_kidoku.py
+++ scripts/all_kidoku.py
@@ -7,10 +7,5 @@ def run():
     for user in users:
         messages_this_user_can_read = Message.objects.filter(Q(years__year=user.year) | Q(years__year=0))
         for message in messages_this_user_can_read:
-            if not Kidoku.objects.filter(message=message).filter(user=user).exists():
-                content = Kidoku(message=message, user=user, have_read=True)
-                content.save()
-            else:
-                content = Kidoku.objects.filter(message=message).get(user=user)
-                content.have_read = True
-                content.save()
+            c = Kidoku.objects.get_or_create(message=message, user=user, have_read=True)
+            print(c)
