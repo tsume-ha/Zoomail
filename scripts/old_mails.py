49f84f161252f0e29202106f4efc59d383a2d7ec
htsume <hashimasa0503@gmail.com>
2019-11-09 16:47:27 +0900

    killされそうなので100件ずつにします

--- scripts/old_mails.py
+++ scripts/old_mails.py
@@ -7,7 +7,7 @@ from members.models import User
 
 def run():
     admin = User.objects.get(email='developer@ku-unplugged.net')
-    for i in range(500):
+    for i in range(100):
         name = str(i) + '.eml'
         target = os.path.join(BASE_DIR, 'scripts', 'emails', name)
         try:
