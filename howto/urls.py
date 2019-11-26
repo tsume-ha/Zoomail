82dbfb4aa9d8123093fea8749dd73079bc13414f
htsume <hashimasa0503@gmail.com>
2019-11-26 17:44:06 +0900

    change urls in 'howto'

--- howto/urls.py
+++ howto/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'howto'
 urlpatterns = [
-    path('', views.index, name='howto_index'),
-    path('user_setting/', views.user_setting,),
-    path('send_message/', views.send_message,),
+    path('', views.index, name='index'),
+    path('user_setting/', views.user_setting, name='user_setting'),
+    path('send_message/', views.send_message, name='send_message'),
 ]
