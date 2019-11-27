1bac53c798770ec341efc505d0512a7515c75b55
htsume <hashimasa0503@gmail.com>
2019-11-27 19:21:48 +0900

    change urls in player

--- player/urls.py
+++ player/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'player'
 urlpatterns = [
-    path('', views.index, name='player_index'),
+    path('', views.index, name='index'),
     path('playlist/<int:live_id>', views.playlist, name='playlist'),
     path('songupload/', views.songupload, name='songupload'),
     path('songupload/<int:FormSetExtraNum>/', views.songupload),
