ec6b3a7b966168fba9ad1f43350ddc75dfad3187
htsume <hashimasa0503@gmail.com>
2019-11-27 19:49:53 +0900

    そのほかのurlを変更

--- otherdocs/urls.py
+++ otherdocs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'otherdocs'
 urlpatterns = [
-    path('', views.index, name='others_index'),
+    path('', views.index, name='index'),
 ]
--- pictures/urls.py
+++ pictures/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'pictures'
 urlpatterns = [
-    path('', views.index, name='pictures_index'),
-    path('register/', views.PhotoRegister, name='pictures_register')
+    path('', views.index, name='index'),
+    path('register/', views.PhotoRegister, name='register')
 ]
