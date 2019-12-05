1f4b5a50cf9188d1d2c66e362e4eab38a0e81ed0
htsume <hashimasa0503@gmail.com>
2019-12-06 00:57:21 +0900

    model作り途中

--- home/models.py
+++ home/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.contenttypes.models import ContentType
 from members.models import User
 
 class SpecialPage(models.Model):
@@ -9,3 +11,27 @@ class SpecialPage(models.Model):
     
     def __str__(self):
         return self.title + ' ' + self.html_name
+
+
+KANSOU = 1
+SOUND = 2
+MOVIE = 3
+ALBUM = 4
+
+MODEL_FLAG_CHOICES = (
+    (KANSOU, 'Kansou'),
+    (SOUND, 'Sound'),
+    (MOVIE, 'Movie'),
+    (ALBUM, 'Album'),
+)
+
+class ContentLog(models.Model):
+    action_time = models.DateTimeField(
+        default=timezone.now,
+        editable=False,
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        models.SET_NULL,
+        blank=True, null=True,
+    )
