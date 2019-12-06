0edfd69d3b99e26888bd0e3f05348421265a7918
htsume <hashimasa0503@gmail.com>
2019-12-06 12:04:53 +0900

    new contentのところはこんなかんじ

--- home/models.py
+++ home/models.py
@@ -10,20 +10,19 @@ class SpecialPage(models.Model):
     def __str__(self):
         return self.title + ' ' + self.html_name
 
-def ContentLog():
+def ContentLog(LIST_NUM=3):
+    from django.urls import reverse
     from kansou.models import Kansouyoushi
     from movie.models import YoutubeURL
     from pictures.models import Album
     from player.models import Performance
 
-    LIST_NUM = 3
-
-    kansou_list = [('kansou', q.created_at, q.live) for q in Kansouyoushi.objects.order_by('-created_at')[:LIST_NUM]]
-    movie_list = [('movie', q.created_at, q.title) for q in YoutubeURL.objects.order_by('-created_at')[:LIST_NUM]]
-    pictures_list = [('pictures', q.created_at, q.title) for q in Album.objects.order_by('-created_at')[:LIST_NUM]]
-    player_list = [('player', q.updated_at, q.live_name) for q in Performance.objects.order_by('-updated_at')[:LIST_NUM]]
+    kansou_list = [('kansou', '感想用紙', q.created_at, q.live, reverse('kansou:index')) for q in Kansouyoushi.objects.order_by('-created_at')[:LIST_NUM]]
+    movie_list = [('movie', 'ライブ映像', q.created_at, q.title, reverse('movie:index')) for q in YoutubeURL.objects.order_by('-created_at')[:LIST_NUM]]
+    pictures_list = [('pictures', '写真', q.created_at, q.title, reverse('pictures:index')) for q in Album.objects.order_by('-created_at')[:LIST_NUM]]
+    player_list = [('player', 'リハ音源', q.updated_at, q.live_name, reverse('player:playlist', args=[q.pk])) for q in Performance.objects.order_by('-updated_at')[:LIST_NUM]]
 
     log_list = kansou_list + movie_list + pictures_list + player_list
-    log_list.sort(key=lambda tup: tup[1], reverse=True)
+    log_list.sort(key=lambda tup: tup[2], reverse=True)
 
     return log_list[:LIST_NUM]
