f62f6da1cda19304e36726560a80c1c897481532
htsume <hashimasa0503@gmail.com>
2019-12-11 19:22:03 +0900

    いけた Vue使ってみた

--- static/js/meeting_room.js
+++ static/js/meeting_room.js
@@ -5,5 +5,18 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function() {
-    console.log(request.response);
+	let data = request.response;
+    let mm = data.date.substr(5,2);
+    let dd = data.date.substr(8,2);
+    let month = Number(mm);
+    let date = Number(dd);
+    let displaydate = month + '/' + date;
+    var meetingroom = new Vue({
+    	el: '#meeting_room',
+    	delimiters: ['[|[', ']|]'],// Vueの{{hoge}}がdjangoとかぶるので、[|[hoge]|]に変えてみます
+    	data: {
+    		today: displaydate,
+    		room: data.room
+    	}
+    })
 }
