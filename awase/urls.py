cd8cea1aed2b6b7ee11646625a9841f9f97ca20f
htsume <hashimasa0503@gmail.com>
2019-12-19 14:46:25 +0900

    urlを変更

--- awase/urls.py
+++ awase/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = 'awase'
 urlpatterns = [
-	path('', views.index, name='awase_index'),
-	path('calendar/<int:pk>/', views.CalendarView, name='awase_calendar'),
+	path('', views.index, name='index'),
+	path('calendar/<int:pk>/', views.CalendarView, name='calendar'),
 	path('calendar/<int:pk>/input/', views.input, name='input_top'),
 	path('calendar/<int:pk>/input/<int:page>/', views.input, name='input'),
-	path('create/', views.create, name='awase_create'),
+	path('create/', views.create, name='create'),
 ]
