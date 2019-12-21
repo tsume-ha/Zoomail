3ac7ccd5b3f34a4c610a023eeb1c3f35c7951612
htsume <hashimasa0503@gmail.com>
2019-12-21 20:10:33 +0900

    例会教室は表示

--- awase/urls.py
+++ awase/urls.py
@@ -5,6 +5,7 @@ app_name = 'awase'
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('calendar/<int:pk>/', views.CalendarView, name='calendar'),
+	path('calendar/json/<int:pk>/', views.CalendarJsonResponse, name='calendar_json'),
 	path('calendar/<int:pk>/input/', views.input, name='input_top'),
 	path('calendar/<int:pk>/input/<int:page>/', views.input, name='input'),
 	path('create/', views.create, name='create'),
---
+++ static/js/calendar_meetingroom.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const meetingroomURL = 'https://meetingroomcontroller.appspot.com/room/all';
+    var request = new XMLHttpRequest();
+    request.open('GET', meetingroomURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function(){
+        let jsondata = request.response;
+        console.log(jsondata);
+        var room = new Vue({
+            el: '#calendar',
+            components:{
+                room:{
+                    props: ['date'],
+                    methods: {
+                        get_room: function(date){
+                            let data = jsondata.filter(function(item, index){
+                                if (item.date == date) return true;
+                            });
+                            if (data.length < 1) {
+                                return 'NoData';
+                            }
+                            let display = data[0].room;
+                            if (display=='終日使用不可') {
+                                display = '使用不可';
+                            }
+                            console.log(display);
+                            return display;
+                        },
+                    },
+                    template: '<div class="room">{{get_room(date)}}</div>',
+                }
+            }
+        });
+    }   
+})
+
+
