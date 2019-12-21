0697aad1c5eb2b5664e8f95d9632fd46333483f1
htsume <hashimasa0503@gmail.com>
2019-12-21 22:01:02 +0900

    動かなくなった...

--- static/js/calendar_display.js
+++ static/js/calendar_display.js
@@ -1,35 +1,14 @@
 function calendar_display(url){
-    var request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function(){
-        let jsondata = request.response;
-        console.log(jsondata);
-        var timedata = new Vue({
-            el: '.day',
-            components:{
-                data: {
-                	json: jsondata
-                },
-                // timedata:{
-                //     props: ['date', 'time'],
-                // },
-                // methods: {
-                //     get_NG_num: function(date, time){
-                //         let num = jsondata[date][time];
-                //         if (typeof(num) === "undefined") {
-                //             console.log('hoge');
-                //             return '';
-                //         } else {
-                //             return ' NG' + String(num);
-                //         }
-                        
-                //     },
-                // },
-                template: '<div class="time"></div>',
-            }
-            
-        });
-    }   
+    // var request = new XMLHttpRequest();
+    // request.open('GET', url);
+    // request.responseType = 'json';
+    // request.send();
+    // request.onload = function(){
+    //     let jsondata = request.response;
+    //     console.log(jsondata);
+    //     var timedata = new Vue({
+    //         el: '.day',
+    //         data: jsondata,            
+    //     });
+    // }
 }
