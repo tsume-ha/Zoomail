f6ad447badb0f3cb019f8ceb9678abff9a29a70e
htsume <hashimasa0503@gmail.com>
2019-12-23 14:46:35 +0900

    途中

--- static/js/calendar_display.js
+++ static/js/calendar_display.js
@@ -12,3 +12,25 @@ function calendar_display(url){
     //     });
     // }
 }
+
+window.addEventListener('DOMContentLoaded', function() {
+
+var data = {
+    calendar_json: null,
+    meetingroom_json: null,
+    days: ['2019-12-23', '2019-12-24', '2019-12-25'],
+}
+
+Vue.component('daycolumns', {
+	props: ['date'],
+	template: '<div class="day column"><div class="date">{{date}}<br>土</div><div class="room">Loading</div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div></div>'
+});
+
+var calendar = new Vue({
+	el: '#calendar',
+	data: data,
+});
+
+})
+
+// <div class="day column" data-date="2019-12-21"><div class="date">12/21<br>土</div><div class="room">Loading</div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div><div class="time"></div></div>
