545bda353fb0af0edf407185b4544dc6703615c5
htsume <hashimasa0503@gmail.com>
2019-12-25 19:56:53 +0900

    js書き途中

--- static/js/calendar_display.js
+++ static/js/calendar_display.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', function() {
 var json_calendar = [
         {date:'Loading', display_date: 'Loading', display_day: '', room: 'Loading', NGlist: ['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',]},
     ];
-
+var json_roomdata;
 
 var calendar = new Vue({
     el: '#calendar',
@@ -11,29 +11,38 @@ var calendar = new Vue({
     data: {
         days: json_calendar,
     },
+    methods: {
+        window:onload = function() {
+            calendar_display(jsonURL, get_room_data);
+            console.log(json_calendar);
+            console.log(json_roomdata);
+        }
+    }
 });
-(function () {
-	function calendar_display(url){
-	    var request = new XMLHttpRequest();
-	    request.open('GET', url);
-	    request.responseType = 'json';
-	    request.send();
-	    request.onload = function(){
-            json_calendar = request.response.calendar_data
-	        calendar.days = json_calendar.slice(0,7);
-console.log(calendar.days);
-	    }
-	}
-	calendar_display('http://localhost:3333/awase/calendar/json/4/');
-}());
 
-(function () {
+
+function calendar_display(url, callback){
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function(){
+        json_calendar = request.response.calendar_data;
+        calendar.days = json_calendar.slice(0,7);
+        callback();
+    }
+}
+
+
+
+function get_room_data(){
     const meetingroomURL = 'https://meetingroomcontroller.appspot.com/room/all';
     var request = new XMLHttpRequest();
     request.open('GET', meetingroomURL);
     request.responseType = 'json';
     request.send();
     request.onload = function(){
+        json_roomdata = request.response;
         for (var i = 0; i < calendar.days.length; i++) {
             let jsondata = request.response.filter(function(item, index){
                 if (item.date == calendar.days[i].date) return true;
@@ -50,7 +59,9 @@ console.log(calendar.days);
             continue;
         }
     }
-}());
+}
 
+
+console.log(jsonURL);
 
 })
