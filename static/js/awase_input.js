6c4037817f88b2f2914a50aa35b8aa3707aa81b7
htsume <hashimasa0503@gmail.com>
2020-01-30 17:54:28 +0900

    一日を一括で入力できるように

--- static/js/awase_input.js
+++ static/js/awase_input.js
@@ -6,8 +6,13 @@ $('label[for*=can_attend]').click(function(){
         attr_list[1] = String(Number(attr_list[1]) + 1);
         var target = attr_list.join('-');
         $('#'+target).prop('checked', true);
-        $('#'+target.slice(0.-2)).val(val);
     }
 })
 
-
+$('.dayall').click(function(e){
+    e.preventDefault();
+    var value = $(this).val();
+    date = $(this).data('date');
+    target = $('div.form_one_day[data-date='+date+'] input[name$=can_attend][value=' + value + ']');
+    target.prop('checked', true);
+})
