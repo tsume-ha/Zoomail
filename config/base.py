0e5cb4ccf00f2520d62cac9b56cc4dc2c1635c36
htsume <hashimasa0503@gmail.com>
2020-03-06 07:54:37 +0900

    現行のsettingsに合わせた

--- config/base.py
+++ config/base.py
@@ -145,6 +145,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.open_id.OpenIdAuth',
     'social_core.backends.google.GoogleOpenId',
     'social_core.backends.google.GoogleOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
 )
 
 SOCIAL_AUTH_LOGIN_URL = '/login/'
