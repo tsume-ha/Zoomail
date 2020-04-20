a67423518ee2c57b74402780ae8eb1e583f05927
htsume <hashimasa0503@gmail.com>
2020-04-21 00:14:18 +0900

    logout まだ出来ないけど

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth import logout
+from django.conf import settings
+from urllib.parse import urlencode
 from django.contrib import messages
 from django.shortcuts import redirect, get_object_or_404
 from .models import SpecialPage, ContentLog, Announcement
@@ -24,8 +26,10 @@ def login(request):
 
 def logoutview(request):
     logout(request)
-    messages.success(request,"ログアウトしました。")
-    return redirect(to='/')
+    return_to = urlencode({'returnTo': request.build_absolute_uri('/')})
+    logout_url = 'https://%s/v2/logout?client_id=%s&%s' % \
+                 (settings.SOCIAL_AUTH_AUTH0_DOMAIN, settings.SOCIAL_AUTH_AUTH0_KEY, return_to)
+    return HttpResponseRedirect(logout_url)
 
 def special(request, url):
     # keyは平文なので安全性は確保できないので注意
