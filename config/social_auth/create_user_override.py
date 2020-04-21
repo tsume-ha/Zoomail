b4988bfc981513bc7fae85f8aaa1efa7eb617b80
htsume <hashimasa0503@gmail.com>
2020-04-21 16:11:50 +0900

    LiveLogでの初回登録時に別の場所に飛べるように変更

--- config/social_auth/create_user_override.py
+++ config/social_auth/create_user_override.py
@@ -1,13 +1,20 @@
+from django.contrib import messages
+
+USER_FIELDS = ['username', 'email']
+
 def create_user(strategy, details, backend, user=None, *args, **kwargs):
     if user:
         return {'is_new': False}
     if backend.name != 'auth0':
+        messages.error(backend.strategy.request, 'あなたのGoogleアカウントは登録されていないため、ログインできませんでいた。')
         return
 
     fields = dict((name, kwargs.get(name, details.get(name)))
                   for name in backend.setting('USER_FIELDS', USER_FIELDS))
     if not fields:
         return
+
+    strategy.session_set('next', '/read')
 
     return {
         'is_new': True,
