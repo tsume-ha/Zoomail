aa1ec23e1e7ea568e2c565bbf89e0c7369f526cb
htsume <hashimasa0503@gmail.com>
2020-05-01 17:02:11 +0900

    編集ファイルの一覧ページを追加

--- otherdocs/urls.py
+++ otherdocs/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('download/<int:pk>/', views.FileDownloadView, name='download'),
     path('upload/', views.UploadView, name='upload'),
+    path('edit/', views.EditIndexView, name='edit_index'),
     path('edit/<int:pk>/', views.EditView, name='edit'),
 ]
--- otherdocs/views.py
+++ otherdocs/views.py
@@ -10,6 +10,7 @@ from .forms import UploadForm, EditForm
 from utils.commom import download
 from config.permissions import OtherDocsPermission
 
+
 @login_required()
 def index(request):
     contents = Content.objects.all().order_by(
@@ -32,7 +33,6 @@ def FileDownloadView(request, pk):
     )
     return response
 
-
 @login_required()
 def UploadView(request):
     now_user = request.user
@@ -54,6 +54,18 @@ def UploadView(request):
     }
     return render(request, 'otherdocs/upload.html', params)
 
+@login_required()
+def EditIndexView(request):
+    now_user = request.user
+    if not OtherDocsPermission(now_user):
+        return redirect(to='otherdocs:index')
+    contents = Content.objects.all().order_by(
+        'index', 'updated_at'
+    ).reverse()
+    params = {
+        'contents': contents,
+    }
+    return render(request, 'otherdocs/edit_index.html', params)
 
 @login_required()
 def EditView(request, pk):
@@ -67,13 +79,14 @@ def EditView(request, pk):
             if form.cleaned_data['delete']:
                 content.delete()
                 messages.success(request, content.title + 'を削除しました')
-                return redirect(to='otherdocs:index')
+                return redirect(to='otherdocs:edit_index')
             now_time = datetime.datetime.now()
             context = form.save(commit=False)
             context.updated_by = now_user
             context.updated_at = now_time
             context.save()
-            return redirect(to='otherdocs:index')
+            messages.success(request, content.title + 'を更新しました')
+            return redirect(to='otherdocs:edit_index')
     params = {
         'form': form
     }
