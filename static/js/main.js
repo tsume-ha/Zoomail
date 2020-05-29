277a6e7586ba789497c7c0f617246e51cbebac44
htsume <hashimasa0503@gmail.com>
2020-05-29 11:57:31 +0900

    base.htmlでnew Vueするdivとしないdivに分けた

--- static/js/main.js
+++ static/js/main.js
@@ -2,21 +2,21 @@ import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueMoment from 'vue-moment'
+
 import MeetingRoom from "../../front/components/meeting-room.vue";
 
 import awaseInput from "../../front/components/awase/input.vue";
 
 Vue.use(VueAxios, axios)
 Vue.use(VueMoment);
-new Vue({
-  el: "#meeting-room",
-  components: {
-    'meeting-room': MeetingRoom
-  }
-})
+
 new Vue({
   el: "#vue-app",
   components: {
+    // home/index 用の例会教室表示コンポーネント
+    'meeting-room': MeetingRoom,
+
+    // awase/calendar/<int:pk>/input/ のコンポーネント
     'awase-input': awaseInput
   }
 })
