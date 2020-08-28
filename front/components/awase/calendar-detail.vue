280e47275e872b91d325239219e4e8d5ded5ee88
htsume <hashimasa0503@gmail.com>
2020-08-28 22:33:55 +0900

    修正

--- front/components/awase/calendar-detail.vue
+++ front/components/awase/calendar-detail.vue
@@ -11,7 +11,6 @@
 
 <script>
 import moment from "moment";
-import func from '../../../vue-temp/vue-editor-bridge';
 moment.locale('ja', {
   weekdays: ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"],
   weekdaysShort: ["日", "月", "火", "水", "木", "金", "土"],
@@ -63,7 +62,7 @@ export default {
         return '○';
       }
     }
-  },
+  }
 }
 </script>
 
