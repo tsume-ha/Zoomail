97f10ed39f9e709076c480ffd75bdd954114dce0
tsume <50492756+tsume-ha@users.noreply.github.com>
2020-09-15 22:49:49 +0900

    jsコードを分割 （遅延ローディング）

--- front/router/index.js
+++ front/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import readIndex from '../components/board/index.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +7,7 @@ const routes = [
   {
     path: '/read/_/',
     name: 'index',
-    component: readIndex
+    component: () => import(/* webpackChunkName: "read" */ '../components/board/index.vue')
   },
 ]
 
