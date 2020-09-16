7f5d707d80cdc243c6661ba3ec0212178b9c0fdc
tsume <50492756+tsume-ha@users.noreply.github.com>
2020-09-16 15:18:24 +0900

    一覧を表示

--- front/components/board/index-one-content.vue
+++ front/components/board/index-one-content.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>{{message.title}}</div>
+  <section class="content row border-bottom">
+    <h4 class="col-12 my-1"><a>{{message.title}}</a></h4>
+    <div class="col-12">
+      <span class="float-left date small p-1">{{message.created_at}}</span>
+      <span class="float-left ml-4 whosent">{{message.writer}}</span>
+      <span class="float-right star"><img src="" width="16" height="16"></span>
+    </div>
+    <article class="col-12 my-2">
+      {{message.content | trim}}
+    </article>
+  </section>
 </template>
 
 <script>
@@ -7,5 +17,23 @@ export default {
   props: {
     message: {type: Object, required: true},
   },
+  filters: {
+    trim (value) {
+      let count = value.indexOf('\n');
+      if (count < 0) {
+        count = 0;
+      }
+      return value.slice(count, count+200);
+    }
+  }
 }
 </script>
+
+<style scoped>
+.content article{
+  display: block;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+</style>
