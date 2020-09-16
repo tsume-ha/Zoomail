a0dfb7060b559c364129274c23d13ce7b5b37f2d
tsume <50492756+tsume-ha@users.noreply.github.com>
2020-09-16 15:25:05 +0900

    bookmarkを表示

--- front/components/board/index-one-content.vue
+++ front/components/board/index-one-content.vue
@@ -4,7 +4,10 @@
     <div class="col-12">
       <span class="float-left date small p-1">{{message.created_at}}</span>
       <span class="float-left ml-4 whosent">{{message.writer}}</span>
-      <span class="float-right star"><img src="" width="16" height="16"></span>
+      <span class="float-right star">
+        <img src="/static/img/star_yl.png" width="16" height="16" v-if="message.is_bookmarked">
+        <img src="/static/img/star_bk.png" width="16" height="16" v-else>
+      </span>
     </div>
     <article class="col-12 my-2">
       {{message.content | trim}}
@@ -19,6 +22,7 @@ export default {
   },
   filters: {
     trim (value) {
+      // 「全回メーリス失礼します」の冒頭を削除
       let count = value.indexOf('\n');
       if (count < 0) {
         count = 0;
