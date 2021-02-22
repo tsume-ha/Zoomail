253c381409eb378d6ca73613cefd2a38bdfe05a5
tsume <50492756+tsume-ha@users.noreply.github.com>
2021-02-23 00:26:03 +0900

    サービスアカウントの場合はcredentialsの書き方が違った

--- meeting_room/service.py
+++ meeting_room/service.py
@@ -3,32 +3,39 @@ import os
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2 import service_account
 from django.conf import settings
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+SCOPES = [
+    'https://www.googleapis.com/auth/calendar.events',
+    ]
 
 def createService():
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    # creds = None
+    # # The file token.pickle stores the user's access and refresh tokens, and is
+    # # created automatically when the authorization flow completes for the first
+    # # time.
     pickle_path = os.path.join(settings.BASE_DIR, 'token.pickle')
     json_path = os.path.join(settings.BASE_DIR, 'credentials.json')
-    print(pickle_path)
-    if os.path.exists(pickle_path):
-        with open(pickle_path, 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                json_path, SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(pickle_path, 'wb') as token:
-            pickle.dump(creds, token)
+    # print(pickle_path)
+    # if os.path.exists(pickle_path):
+    #     with open(pickle_path, 'rb') as token:
+    #         creds = pickle.load(token)
+    # # If there are no (valid) credentials available, let the user log in.
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(
+    #             json_path, SCOPES)
+    #         creds = flow.run_local_server(port=0)
+    #     # Save the credentials for the next run
+    #     with open(pickle_path, 'wb') as token:
+    #         pickle.dump(creds, token)
 
-    return build('calendar', 'v3', credentials=creds)
+
+    credentials = service_account.Credentials.from_service_account_file(
+            json_path, scopes=SCOPES)
+
+    return build('calendar', 'v3', credentials=credentials)
