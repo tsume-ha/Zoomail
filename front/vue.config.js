5d58c93ac19317a4133a7b03981a188e0fff9095
tsume_ha <50492756+tsume-ha@users.noreply.github.com>
2021-09-01 00:06:26 +0900

    serveしてホットリロードはできた

---
+++ front/vue.config.js
@@ -0,0 +1,33 @@
+var BundleTracker = require("webpack-bundle-tracker");
+// var VueLoaderPlugin = require("vue-loader/lib/plugin");
+// var MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+
+module.exports = {
+    //ここで指定した場所で展開する
+    outputDir: '../',
+    //サーバーを起動した時のルートパス
+    publicPath: process.env.NODE_ENV === 'production' 
+        ? '/static/dist/'
+        : 'http://localhost:8080/',
+    // //outputDir起点でindex.htmlを格納する場所を指定
+    // indexPath: 'templates/index.html',
+    //outputDir起点でstaticファイルを格納する場所を指定
+    assetsDir: 'static/dist',
+
+    configureWebpack: {
+        plugins: [
+          new BundleTracker({ filename: "../static/dist/webpack-stats.json" }),
+        //   new VueLoaderPlugin(),
+        //   new MomentLocalesPlugin({ localesToKeep: ['ja'] }),
+        //   new BundleAnalyzerPlugin(),
+        ],
+    },
+    css: {
+      sourceMap: true,
+    },
+    devServer: {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
+}
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('_/', TemplateView.as_view(template_name='index.html')),
     path('first_register/', views.firstRegister, name='first-register'),
 ]
