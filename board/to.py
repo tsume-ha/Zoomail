5bf6b8f2b3b767ebd8b738f4d903b12e5e3525ec
tsume_ha <50492756+tsume-ha@users.noreply.github.com>
2021-09-08 14:44:16 +0900

    並び順を変更

--- board/to.py
+++ board/to.py
@@ -2,6 +2,8 @@ from .models import To
 from .to_label import label
 
 to_groups = [
-    (item.year, label(item)) for item in To.objects.all().order_by("year")
+    (item.year, label(item)) for item in To.objects.filter(year=0)
+] + [
+    (item.year, label(item)) for item in To.objects.filter(year__gt=0).order_by("year").reverse()
 ]
 
