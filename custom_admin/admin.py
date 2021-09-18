b630c5f016b0feca79681575c60f87ce3a1808e8
tsume-ha <50492756+tsume-ha@users.noreply.github.com>
2021-09-18 15:16:52 +0900

    soundのadminページを作成

--- custom_admin/admin.py
+++ custom_admin/admin.py
@@ -3,5 +3,4 @@ from django.contrib.admin import AdminSite
 class CustomAdminSite(AdminSite):
     site_header = 'Zoomail 管理者用サイト'
 
-admin_site = CustomAdminSite(name='custom_admin')
-
+custom_admin_site = CustomAdminSite(name='custom_admin')
--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
 from django.views.generic.base import TemplateView
-from custom_admin.admin import admin_site
+from custom_admin.admin import custom_admin_site
 from . import views as home
 import private_storage.urls
 
@@ -16,7 +16,7 @@ admin.site.has_permission = has_permission
 
 urlpatterns = [
     path('db/', admin.site.urls),
-    path('admin/', admin_site.urls),
+    path('admin/', custom_admin_site.urls),
     path('auth/', include('social_django.urls', namespace='social')),
     path('private-media/', include(private_storage.urls), name="private_media"),
     
