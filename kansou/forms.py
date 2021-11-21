b7b234c9f30ca0fe9325cc67e270fc5a8c81f62f
tsume-ha <50492756+tsume-ha@users.noreply.github.com>
2021-11-21 23:12:40 +0900

    kansouモデルを修正 タイトルを選択式から記述式に変更

--- kansou/forms.py
+++ kansou/forms.py
@@ -6,41 +6,24 @@ import datetime
 class KansouUploadForm(forms.ModelForm):
     class Meta:
         model = Kansouyoushi
-        fields = ['live', 'performed_at', 'numbering', 'file', 'detail']
+        fields = ['title', 'performed_at', 'file', 'detail']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs["class"] = "form-control"
-        self.fields['live'] = forms.ChoiceField(
-            choices = Kansouyoushi.livename,
-            label = "ライブ名",
-            widget = forms.Select(attrs={'class': 'form-control'}),
-            required = True,
-            )
+        self.fields['title'].widget.attrs['placeholder'] = '例）新歓ライブ#1'
         self.fields['file'].widget.attrs["class"] = "m-2"
         self.fields['detail'].required = False
         self.fields['detail'].widget.attrs['placeholder'] = '例）1回生の分です'
-        self.fields['numbering'].required = False
         self.fields['performed_at'].initial = datetime.date.today()
         self.fields['performed_at'].widget = forms.DateInput(attrs={'type': 'date', 'class': 'form-control',})
-
-
-    def clean_numbering(self):
-        numbering = self.cleaned_data['numbering']
-        if numbering == None:
-            numbering = 1
-        return numbering
 
     def clean(self):
         cleaned_data = super().clean()
         performed_at = cleaned_data.get("performed_at")
-        livename = cleaned_data.get("live")
         file = cleaned_data.get("file")
-        numbering = cleaned_data.get('numbering')
-        filename = performed_at.strftime('%Y_%m_%d') + '_' + livename
-        if numbering != None:
-            filename += '_' + str(numbering)
+        filename = performed_at.strftime('%Y_%m_%d_%M_%S')
         filename += '.pdf'
         file.name = filename
         return cleaned_data
