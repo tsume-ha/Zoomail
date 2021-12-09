58a9955e578b832d515e3a4e4d315d540c155bba
tsume-ha <50492756+tsume-ha@users.noreply.github.com>
2021-12-09 21:22:26 +0900

    googleフォントを導入した

--- front/src/private.js
+++ front/src/private.js
@@ -3,4 +3,6 @@ import App from "./PrivateApp.vue";
 import store from "./store";
 import router from "./router";
 
+import "@/assets/sass/main.scss";
+
 createApp(App).use(router).use(store).mount("#app");
