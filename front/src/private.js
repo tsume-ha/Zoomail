b8ba4bbf3ce55c5739cb4592aa6a527b34b0f21f
tsume-ha <50492756+tsume-ha@users.noreply.github.com>
2021-12-14 23:43:47 +0900

    font awesomeを導入 <font-awesome-icon icon="user-secret" />の形で使える

--- front/src/private.js
+++ front/src/private.js
@@ -3,6 +3,12 @@ import App from "./PrivateApp.vue";
 import store from "./store";
 import router from "./router";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faUserSecret);
+
+
 import "@/assets/sass/main.scss";
 
-createApp(App).use(router).use(store).mount("#app");
+createApp(App).use(router).use(store).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
