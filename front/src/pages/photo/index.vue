8dfc7eb218d629ea1bea2ea54bc37c08cf7b967d
tsume-ha <50492756+tsume-ha@users.noreply.github.com>
2021-12-26 23:58:26 +0900

    photoを作製

--- front/src/pages/photo/index.vue
+++ front/src/pages/photo/index.vue
@@ -1,26 +1,28 @@
 <template>
-  <section>
+  <article>
     <h3>写真</h3>
-    <p class="py-2">
+    <p>
       アンプラのイベントの写真を公開しています。<br>
-      LiveLogに登録されていないBBQやお花見のイベントの写真もあります！
+      LiveLogに登録されていないBBQやお花見のイベントの写真もあります!
     </p>
-    <div class="row" v-if="photoStaff" v-cloak>
-      <a href="/admin/pictures/album/">アルバムの登録・編集はこちらから</a>
-    </div>
-    <div v-for="y in yearsSet" :key="y" class="row">
-      <h4 class="col-12">{{y}}年度</h4>
-      <div v-for="item in getItems(y)" :key="item.id" class="cardwrap col-6 col-xs-6 col-sm-6 col-md-4 col-lg-3 my-2">
-        <article class="card">
-          <img class="card-img-top" :src="thumbnailPath(item)" />
-          <a :href="item.url" class="card-img-overlay" target="_blank">
-            <h5 class="card-title" v-text="item.title"></h5>
-            <span>{{ displayDate(item.date) }}</span>
-          </a>
-        </article>
+    <div class="pure-g">
+      <div
+        v-for="photo in photos" :key="'photo-' + photo.id"
+        class="pure-u-1-2 pure-u-sm-1-3 pure-u-md-1-4 pure-u-lg-1-5 card-wrapper"
+      >
+        <a :href="photo.url" class="card-photo" target="_blank">
+          <img v-if="photo.thumbnail" :src="photo.thumbnail"
+            :alt="`${photo.title} - サムネイル`"
+            class="pure-img" />
+          <img v-else src="@/assets/img/unplugged_logo.jpg"
+            :alt="`${photo.title} - サムネイル`"
+            class="pure-img" />
+          <span class="photo-title">{{ photo.title }}</span>
+          <span class="photo-date">{{ displayDate(photo.date) }}</span>
+        </a>
       </div>
     </div>
-  </section>
+  </article>
 </template>
 
 <script>
@@ -89,45 +91,60 @@ export default {
 };
 </script>
 
-<style scoped>
-a.card-img-overlay{
-  border-radius: 3px;
-  color: #fff;
-  background-color: rgba(0,0,0,0.3);
-  transition: 0.4s;
-}
+<style lang="scss" scoped>
+.card-wrapper {
+  padding: 1rem .5rem;
 
-a.card-img-overlay:after{
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 3px;
-  box-shadow: inset 0 0 0px 5px rgba(255,255,255,0.5);
-}
+  .card-photo {
+    display: inline-block;
+    position: relative;
+    border-radius: 3px;
+    color: #FFF;
+    transition: 0.4s;
 
-a.card-img-overlay.hover{
-  text-decoration: none;
-  color: #000;
-  background-color: rgba(255,255,255,0.5);
-}
+    &:hover {
+      text-decoration: none;
+      color: #000;
+      &::after {
+        background-color: rgba(255,255,255,1);
+        box-shadow: inset 0 0 0px 5px rgba(0,0,0,0.8);
+      }
+    }
+    &::after{
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      border-radius: 3px;
+      background-color: rgba(0,0,0,0.8);
+      box-shadow: inset 0 0 0px 5px rgba(255,255,255,1);
+      opacity: 0.3;
+      transition: background-color 0.4s;
+      z-index: 1;
+    }
 
-h6.card-subtitle{
-  position: absolute;
-  right: 1rem;
-  bottom: 1rem;
-  font-size: 0.75rem;
-  font-weight: 400;
-  color: #fff;
-  transition: 0.4s;
-}
-a.card-img-overlay.hover h6.card-subtitle{
-  color: #000;
-  text-decoration: none;
-}
-[v-cloak] {
-  display: none;
+    img {
+      position: relative;
+      border-radius: 3px;
+    }
+    .photo-title, .photo-date {
+      position: absolute;
+      z-index: 2;
+      max-width: calc(100% - 10px - 1.5rem);
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    .photo-title {
+      top: .75rem;
+      left: .75rem;
+    }
+    .photo-date {
+      bottom: .75rem;
+      right: .75rem;
+      font-size: .75rem;
+    }
+  }
 }
 </style>
--- pictures/views.py
+++ pictures/views.py
@@ -1,5 +1,4 @@
 from django.http.response import JsonResponse
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from .models import Album
