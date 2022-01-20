d9307fcecbbc0cc625c0d8c1c7fb45c0683bd567
tsume-ha <50492756+tsume-ha@users.noreply.github.com>
2022-01-20 14:18:45 +0900

    ページネーションのスタイルを修正

--- front/src/components/Paginator.vue
+++ front/src/components/Paginator.vue
@@ -1,31 +1,34 @@
 <template>
   <ul>
-    <li @click="clicked(page-1)" :class="{disabled: page === 1}">
-      <Icon icon="angle-left" /></li>
-    <li @click="clicked(1)" v-if="page > 1">
-      1</li>
-    <li class="circle" v-if="((page-1) - 1) > 1">
-      </li>
-    <li @click="clicked(page-1)" v-if="page-1 > 1">
-      {{page-1}}</li>
+    <li @click="clicked(page - 1)" :class="{ disabled: page === 1 }">
+      <Icon icon="angle-left" />
+    </li>
+    <li @click="clicked(1)" v-if="page > 1">1</li>
+    <li class="circle" v-if="page - 1 - 1 > 1"></li>
+    <li @click="clicked(page - 1)" v-if="page - 1 > 1">
+      {{ page - 1 }}
+    </li>
     <li @click="clicked(page)" class="active">
-      {{page}}</li>
-    <li @click="clicked(page+1)" v-if="page+1 < pages">
-      {{page+1}}</li>
-    <li class="circle" v-if="(pages - (page+1)) > 1">
-      </li>
+      {{ page }}
+    </li>
+    <li @click="clicked(page + 1)" v-if="page + 1 < pages">
+      {{ page + 1 }}
+    </li>
+    <li class="circle" v-if="pages - (page + 1) > 1"></li>
     <li @click="clicked(pages)" v-if="page < pages">
-      {{pages}}</li>
-    <li @click="clicked(page+1)" :class="{disabled: page === pages}">
-      <Icon icon="angle-right" /></li>
+      {{ pages }}
+    </li>
+    <li @click="clicked(page + 1)" :class="{ disabled: page === pages }">
+      <Icon icon="angle-right" />
+    </li>
   </ul>
 </template>
 
 <script>
 export default {
   props: {
-    pages: {required: true, type: Number},
-    page: {required: true, type: Number}
+    pages: { required: true, type: Number },
+    page: { required: true, type: Number },
   },
   // setup(_props, context) {
   //   const clicked = page => {
@@ -36,10 +39,10 @@ export default {
   //   };
   // },
   methods: {
-    clicked (pageTo) {
+    clicked(pageTo) {
       this.$emit("pageTo", pageTo);
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -56,21 +59,22 @@ ul {
   li {
     list-style: none;
     display: inline-block;
-    box-sizing: content-box;
+    box-sizing: border-box;
     text-align: center;
-    margin: .5rem;
-    padding: .4rem;
-    width: 1rem;
-    height: 1rem;
+    margin: 0.5rem;
+    padding: 0.65rem 0.4rem;
+    min-width: 2.5rem;
+    min-height: 2.5rem;
     line-height: 100%;
+    font-size: 1rem;
     border-radius: 50%;
     background-color: $bg-light-lighten3;
     cursor: pointer;
 
     &.circle {
-      width: .2rem;
-      height: .2rem;
-    padding: .1rem;
+      min-width: 0.4rem;
+      min-height: 0.4rem;
+      padding: 0.1rem;
       cursor: default;
     }
     &.disabled {
