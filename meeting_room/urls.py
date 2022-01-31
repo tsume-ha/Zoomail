3464985177fe0a84fa6c4d6bd5e70b16762d4280
tsume-ha <50492756+tsume-ha@users.noreply.github.com>
2022-02-01 01:00:26 +0900

    api提供部分を再実装

--- meeting_room/urls.py
+++ meeting_room/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 app_name = "meeting-room"
 urlpatterns = [
-    path("meeting_room.ics", views.EventFeed()),
+    path("meeting_room.ics", views.CalendarFeed()),
     # path("", views.index),
     # path("register/", views.register, name="register"),
     # path("sync/", views.sync, name="sync"),
     # path("", views.index),
-    # path("get31day/", views.get31day),
-    # path("today/", views.today),
+    path("get31day/", views.get31day),
+    path("today/", views.today),
     # path("get_all/", views.get_all),
 ]
--- meeting_room/views.py
+++ meeting_room/views.py
@@ -1,11 +1,12 @@
 import datetime
-
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
 from django_ical.views import ICalFeed
 
 from .models import Room
 
 
-class EventFeed(ICalFeed):
+class CalendarFeed(ICalFeed):
     """
     A simple event calender
     """
@@ -28,3 +29,35 @@ class EventFeed(ICalFeed):
 
     def item_link(self, item):
         return "/meeting_room/"
+
+
+def today(request):
+    today = datetime.date.today()
+    try:
+        room = Room.objects.get(date=today).room_name
+    except ObjectDoesNotExist:
+        room = None
+    return JsonResponse({
+        "room": room,
+        "date": today.strftime("%Y-%m-%d")
+    })
+
+def getRoomsByRange(start_date, end_date):
+    room_list = Room.objects.filter(date__gte=start_date, date__lte=end_date).order_by("date").values("date", "room_name")
+    date_list = [start_date + datetime.timedelta(days=i) for i in range((end_date - start_date).days + 1)]
+    return [{
+        "date": date.strftime("%Y-%m-%d"),
+        "room": next(
+            filter(lambda item: item["date"] == date, room_list),
+            {"room_name": None}
+        )["room_name"]
+    } for date in date_list]
+
+def get31day(request):
+    today = datetime.date.today()
+    return JsonResponse({
+        "rooms": getRoomsByRange(
+            start_date = today,
+            end_date = today + datetime.timedelta(days=30)
+        )
+    })
