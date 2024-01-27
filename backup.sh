e487f97fe64979c7e62de841607cfe221d78cfb4
tsume-ha <tsume-ha@protonmail.com>
2024-01-27 15:10:17 +0900

    fix backup script

--- backup.sh
+++ backup.sh
@@ -4,16 +4,22 @@
 now=$(date +"%Y%m%d_%H%M")
 
 # create zoomail_db_dump directory if it doesn't exist
-mkdir -p /zoomail_db_dump/
+mkdir -p ~/zoomail_db_dump
 
 # dump the database
-docker compose -f compose.prod.yaml exec database mariadb-dump --all-databases -uroot -p"$DATABASE_PASSWORD" > /zoomail_db_dump/zoomail_$now.sql
+docker compose -f compose.prod.yaml exec -T database mariadb-dump --all-databases -uroot -p$DATABASE_PASSWORD > ~/zoomail_db_dump/zoomail_$now.sql
+
+cd ~/zoomail_db_dump
 
 # compress the database dump
-tar -czvf /zoomail_db_dump/zoomail_$now.tar.gz /zoomail_db_dump/zoomail_$now.sql
+tar -czvf zoomail_$now.tar.gz zoomail_$now.sql
 
 # remove the uncompressed database dump
-rm /zoomail_db_dump/zoomail_$now.sql
+rm zoomail_$now.sql
+
+chmod 600 zoomail_$now.tar.gz
+
+cd -
 
 # build the awscli docker image
 docker image build -t awscli -f aws-cli.Dockerfile .
@@ -22,7 +28,7 @@ docker image build -t awscli -f aws-cli.Dockerfile .
 docker run \
     --rm \
     -it \
-    -v /zoomail_db_dump:/zoomail_db_dump \
+    -v ~/zoomail_db_dump:/zoomail_db_dump \
     -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
