a5e86b2807531a9608377147fded400e32d99984
tsume-ha <tsume-ha@protonmail.com>
2024-12-14 14:12:01 +0900

    private topページを作成した

--- top/views.py
+++ top/views.py
@@ -4,7 +4,22 @@ from django.contrib import messages
 
 def top_page_view(request):
     if request.user.is_authenticated:
-        return render(request, "top/private.html")
+        new_contents = [
+            "An item",
+            "A second item",
+            "A third item",
+            "A fourth item",
+            "And a fifth one",
+        ]
+        announcements = [
+            "Announcement 1",
+            "Announcement 2",
+            "Announcement 3",
+            "Announcement 4",
+            "Announcement 5",
+        ]
+        context = {"new_contents": new_contents, "announcements": announcements}
+        return render(request, "top/private.html", context)
     else:
         if "loggedout" in request.GET:
             messages.success(request, "ログアウトが完了しました")
