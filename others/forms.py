856125d0e9f8800afd8c9636301418acb6998962
tsume-ha <tsume-ha@protonmail.com>
2025-03-24 22:55:13 +0900

    ファイル名変更を追加

--- others/forms.py
+++ others/forms.py
@@ -23,6 +23,28 @@ class FileUploadForm(forms.ModelForm):
 
 
 class FileEditForm(forms.ModelForm):
+    filename = forms.CharField(label="ファイル名", max_length=255)
+
     class Meta:
         model = File
         fields = ["file", "filename"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file_ext = ""
+        if self.instance and getattr(self.instance, "pk", None):
+            full_name = self.instance.filename
+            basename, dot, ext = full_name.rpartition(".")
+            if dot:
+                self.initial["filename"] = basename
+                self.file_ext = "." + ext
+            else:
+                self.file_ext = ""
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        input_basename = self.cleaned_data.get("filename", "")
+        instance.filename = input_basename + self.file_ext
+        if commit:
+            instance.save()
+        return instance
