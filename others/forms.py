491a4240166bef2d6cce19748ff2036b956875ab
tsume-ha <tsume-ha@protonmail.com>
2025-03-24 22:42:48 +0900

    テンプレートのスタイルを修正

--- others/forms.py
+++ others/forms.py
@@ -5,11 +5,18 @@ from .models import File
 class FileUploadForm(forms.ModelForm):
     class Meta:
         model = File
-        fields = ["file", "filename"]
+        fields = [
+            "file",
+        ]
 
-    def clean(self):
-        cleaned_data = super().clean()
-        uploaded_file = cleaned_data.get("file")
+    def save(self, commit=True):
+        # インスタンス生成（commit=Falseにより保存前の状態）
+        instance = super().save(commit=False)
+        # リクエストから渡されたアップロードファイルを取得
+        uploaded_file = self.files.get("file")
         if uploaded_file:
-            cleaned_data["filename"] = uploaded_file.name
-        return cleaned_data
+            # ここでアップロード時のオリジナルのファイル名（日本語が含まれる）を取得
+            instance.filename = uploaded_file.name
+        if commit:
+            instance.save()
+        return instance
