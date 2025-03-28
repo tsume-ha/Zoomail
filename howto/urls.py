1dc2e45b7519d3bfd9bfa644f8c8f648c53a14c5
tsume-ha <tsume-ha@protonmail.com>
2025-03-28 13:41:48 +0900

    create howto pages by gpt

--- howto/urls.py
+++ howto/urls.py
@@ -1,8 +1,18 @@
-from django.views.generic import TemplateView
 from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = "howto"
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="howto/index.html"), name="index"),
+    path(
+        "hajimeni/",
+        TemplateView.as_view(template_name="howto/hajimeni.html"),
+        name="hajimeni",
+    ),
+    path(
+        "login/",
+        TemplateView.as_view(template_name="howto/login.html"),
+        name="login",
+    ),
 ]
-
-app_name = "howto"
