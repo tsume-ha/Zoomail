32d6b22a67a3c538c8ab71efc1d5f75843102b0c
tsume-ha <tsume-ha@protonmail.com>
2026-06-09 22:31:49 +0900

    管理者権限をsignalで表現する実装に戻した

--- custom_admin/admin.py
+++ custom_admin/admin.py
@@ -9,53 +9,6 @@ from .forms import GroupAdminForm
 class CustomAdminSite(admin.AdminSite):
     site_header = "Zoomail 管理者用ページ"
 
-    class StaffPermissionAdminMixin:
-        def _has_explicit_permission_override(self, method_name):
-            for cls in type(self).mro()[2:]:
-                if cls is admin.ModelAdmin:
-                    return False
-                if method_name in cls.__dict__:
-                    return True
-            return False
-
-        def _has_custom_admin_permission(self, request):
-            return request.user.is_active and request.user.is_staff
-
-        def has_module_permission(self, request):
-            if self._has_explicit_permission_override("has_module_permission"):
-                return super().has_module_permission(request)
-            return self._has_custom_admin_permission(request)
-
-        def has_view_permission(self, request, obj=None):
-            if self._has_explicit_permission_override("has_view_permission"):
-                return super().has_view_permission(request, obj)
-            return self._has_custom_admin_permission(request)
-
-        def has_change_permission(self, request, obj=None):
-            if self._has_explicit_permission_override("has_change_permission"):
-                return super().has_change_permission(request, obj)
-            return self._has_custom_admin_permission(request)
-
-        def has_add_permission(self, request):
-            if self._has_explicit_permission_override("has_add_permission"):
-                return super().has_add_permission(request)
-            return self._has_custom_admin_permission(request)
-
-        def has_delete_permission(self, request, obj=None):
-            if self._has_explicit_permission_override("has_delete_permission"):
-                return super().has_delete_permission(request, obj)
-            return self._has_custom_admin_permission(request)
-
-    def register(self, model_or_iterable, admin_class=None, **options):
-        admin_class = admin_class or admin.ModelAdmin
-        if not issubclass(admin_class, self.StaffPermissionAdminMixin):
-            admin_class = type(
-                f"Custom{admin_class.__name__}",
-                (self.StaffPermissionAdminMixin, admin_class),
-                {"__module__": admin_class.__module__},
-            )
-        return super().register(model_or_iterable, admin_class, **options)
-
     def password_change(self, request, extra_context=None):
         messages.error(request, "Zoomailではパスワードを設定しません")
         return render(request, "registration/password_change_form.html")
--- members/tests/test_custom_admin_permissions.py
+++ members/tests/test_custom_admin_permissions.py
@@ -1,13 +1,22 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 from django.urls import reverse
 
+from members.signals import ADMIN_GROUP_NAME
 from photo.models import PhotoAlbum
 
 
 class CustomAdminStaffPermissionTests(TestCase):
     @classmethod
     def setUpTestData(cls):
+        cls.admin_group = Group.objects.create(name=ADMIN_GROUP_NAME)
+        photo_album_content_type = ContentType.objects.get_for_model(PhotoAlbum)
+        cls.admin_group.permissions.set(
+            Permission.objects.filter(content_type=photo_album_content_type)
+        )
+
         cls.staff_user = get_user_model().objects.create_user(
             email="custom-admin-staff@example.com", year=2024
         )
@@ -19,13 +28,16 @@ class CustomAdminStaffPermissionTests(TestCase):
     def setUp(self):
         self.client.force_login(self.staff_user)
 
-    def test_staff_user_can_access_custom_admin_model_without_auth_permissions(self):
+    def test_staff_user_is_added_to_admin_group(self):
+        self.assertTrue(self.staff_user.groups.filter(name=ADMIN_GROUP_NAME).exists())
+
+    def test_staff_user_can_access_custom_admin_model_with_group_permissions(self):
         response = self.client.get(reverse("custom_admin:photo_photoalbum_changelist"))
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "アルバム")
 
-    def test_staff_user_can_add_custom_admin_model_without_auth_permissions(self):
+    def test_staff_user_can_add_custom_admin_model_with_group_permissions(self):
         response = self.client.post(
             reverse("custom_admin:photo_photoalbum_add"),
             data={
@@ -40,7 +52,8 @@ class CustomAdminStaffPermissionTests(TestCase):
         self.assertEqual(album.title, "新歓ライブ")
         self.assertEqual(album.created_by, self.staff_user)
 
-    def test_staff_user_cannot_access_developer_admin_model_without_auth_permissions(self):
-        response = self.client.get(reverse("admin:photo_photoalbum_changelist"))
+    def test_user_is_removed_from_admin_group_when_staff_is_removed(self):
+        self.staff_user.is_staff = False
+        self.staff_user.save()
 
-        self.assertEqual(response.status_code, 403)
+        self.assertFalse(self.staff_user.groups.filter(name=ADMIN_GROUP_NAME).exists())
